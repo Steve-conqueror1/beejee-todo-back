496b7e76081068acab0cdc4170e5fc958d521c17
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2022-02-26 23:43:51 +0300

    Edited seed script

--- src/seeds/seedDb.js
+++ src/seeds/seedDb.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
-const url = `${process.env.DATABASE_URI}`
+const url = `${process.env.MONGODB_URI}`
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     if (err) {
