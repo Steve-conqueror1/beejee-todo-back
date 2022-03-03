0e79938f0cbd160dc613484fb432a1ce7d89de8c
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2022-03-03 19:56:11 +0300

    Added pagination to the Tasks

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -4,13 +4,21 @@ const { Task } = require('../models');
 module.exports = {
   ...genericCrud(Task),
     async getAll(req, res) {
+    const limit = req.query.limit || 3
+      const pageNumber = req.query.page || 1
+      const count = await Task.countDocuments()
+
     try {
-      const data = await Task.find().populate('createdBy', ['email', 'username']).then(items => {
-      return items;
+      const data =await Task.find()
+          .skip( pageNumber > 0 ? ( ( pageNumber - 1 ) * limit ) : 0 )
+          .limit(limit)
+          .populate('createdBy', ['email', 'username'])
+          .then(items => {
+            return items;
       })
-      return res.status(200).send(data)
+      return res.status(200).json({data: data, documentCount: count, totalPages: Math.ceil(count/limit)})
     } catch (err) {
-      return res.status(400).send(boom.boomify(err));
+      return res.status(400).send(err);
     }
   },
 };
